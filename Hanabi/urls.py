0b8d91fc7c5db6b1fcde3bd6d0f59dbbb7a4475d
Colin Eyster <cjegy9@mst.edu>
2016-04-20 10:28:17 -0500

    create games url

--- Hanabi/urls.py
+++ Hanabi/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns(
     url(r'Statistics/$', views.statistics, name='statistics'),
     url(r'Rules/$', views.rules, name='rules'),
     url(r'index/$', views.index, name='index'),
-    url(r'create_game/', views.create_game, name='create_game'),
+    url(r'create_game/$', views.create_game, name='create_game'),
 )
 urlpatterns += patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
